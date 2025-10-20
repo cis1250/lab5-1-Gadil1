a35e262d40b74b0a65686ea17c41e54b831f84c3
Gadil alabi <galabi@uoguelph.ca>
2025-10-20 12:34:38 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
-# Fibonacci Sequence Exercise with functions
-# TODO: (Read detailed instructions in the Readme file)
+def get_positive_integer():
+    n = input("Enter the number of terms: ")
+    while not n.isdigit() or int(n) <= 0:
+        print("Please enter positive integer.")
+        n = input("Enter the number of terms: ")
+    return int(n)
+
+def fibonnaci_sequence(n):
+    a, b = 0, 1
+    sequence = []
+    for i in range(n):
+      sequence.append(a)
+      a, b = b, a + b
+    return sequence
+
+def print_fibonnaci(sequence):
+  for num in sequence:
+    print (num, end=" ")
+  print()
+  
+    
+        
+def main():
+    n = get_positive_integer()
+    print("Fibonnaci sequence:")
+    print_fibonnaci(n)
+
+main()
